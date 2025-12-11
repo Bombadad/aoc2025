0db026838f9d516c486f56177bcf90277e9cfd4a
Caleb Baum <caleb.baum@i-labs.tech>
2025-12-11 14:26:06 -0500

    Start on day 9

--- src/solutions/day_09.rs
+++ src/solutions/day_09.rs
@@ -15,6 +15,11 @@ impl Point {
     }
 }
 
+struct Edge {
+    first: Point,
+    end: Point,
+}
+
 pub fn solve() -> u64 {
     let mut areas = INPUT
         .lines()
@@ -33,4 +38,24 @@ pub fn solve() -> u64 {
     areas.sort();
     // println!("{:?}", areas);
     return areas.last().unwrap().to_owned().try_into().unwrap();
+}
+
+pub fn solve_2() -> u64 {
+    let mut areas = TEST_INPUT
+        .lines()
+        .map(|l| {
+            let (x, y) = l.split_once(",").unwrap();
+
+            Point {
+                x: x.parse().unwrap(),
+                y: y.parse().unwrap(),
+            }
+        })
+        .collect_vec();
+
+    // let mut
+
+    // lets make the edges
+    // println!("{:?}", areas);
+    return 0;
 }
